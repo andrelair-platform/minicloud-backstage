68cb70e0d8044313e8072dcd14e482bf014ff1ca
AndreLiar <andrelaurelyvan.kanmegnetabouguie@ynov.com>
2026-07-10 21:48:14 +0200

    feat(backstage): enable Kubernetes plugin for live pod view on catalog entities

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,8 +1,9 @@
 import { createApp } from '@backstage/frontend-defaults';
 import catalogPlugin from '@backstage/plugin-catalog/alpha';
+import kubernetesPlugin from '@backstage/plugin-kubernetes/alpha';
 import { navModule } from './modules/nav';
 import { authModule } from './modules/auth';
 
 export default createApp({
-  features: [catalogPlugin, navModule, authModule],
+  features: [catalogPlugin, kubernetesPlugin, navModule, authModule],
 });
