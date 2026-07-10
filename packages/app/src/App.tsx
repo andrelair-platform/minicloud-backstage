344fa2dd4ccb0723f2740376d6e9693a595e0d3a
AndreLiar <andrelaurelyvan.kanmegnetabouguie@ynov.com>
2026-07-10 22:38:58 +0200

    Add tech-radar plugin with platform technology landscape data

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -2,9 +2,10 @@ import { createApp } from '@backstage/frontend-defaults';
 import catalogPlugin from '@backstage/plugin-catalog/alpha';
 import apiDocsPlugin from '@backstage/plugin-api-docs/alpha';
 import kubernetesPlugin from '@backstage/plugin-kubernetes/alpha';
+import techRadarPlugin from '@backstage/plugin-tech-radar/alpha';
 import { navModule } from './modules/nav';
 import { authModule } from './modules/auth';
 
 export default createApp({
-  features: [catalogPlugin, apiDocsPlugin, kubernetesPlugin, navModule, authModule],
+  features: [catalogPlugin, apiDocsPlugin, kubernetesPlugin, techRadarPlugin, navModule, authModule],
 });
