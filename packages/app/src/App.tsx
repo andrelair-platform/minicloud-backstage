ceaac45e781039eff9fb6b28d2103fb3d337b085
AndreLiar <andrelaurelyvan.kanmegnetabouguie@ynov.com>
2026-07-10 22:28:06 +0200

    Add api-docs plugin to enable API tab and Swagger UI on catalog entity pages

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,9 +1,10 @@
 import { createApp } from '@backstage/frontend-defaults';
 import catalogPlugin from '@backstage/plugin-catalog/alpha';
+import apiDocsPlugin from '@backstage/plugin-api-docs/alpha';
 import kubernetesPlugin from '@backstage/plugin-kubernetes/alpha';
 import { navModule } from './modules/nav';
 import { authModule } from './modules/auth';
 
 export default createApp({
-  features: [catalogPlugin, kubernetesPlugin, navModule, authModule],
+  features: [catalogPlugin, apiDocsPlugin, kubernetesPlugin, navModule, authModule],
 });
